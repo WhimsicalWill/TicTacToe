1d46b68bece3fffc43856d0450a62df892690f2b
WhimsicalWill <knipewilliam20@gmail>
2015-11-02 17:13:29 -0500

    Added Minimax Algorithm

--- game.js
+++ game.js
@@ -1,15 +1,19 @@
 var game = new Phaser.Game(1000, 660, Phaser.AUTO, '', {preload: preload, create: create, update: update});
 
-var moveCount = 0;
 //var player = new Player("x");
 var playerTurn;
-var sprite, sprite2;
+var bestMove;
+var MAX_DEPTH = 6;
+
 
 var EMPTY_SPACE = " ";
 var board = [[EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
              [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
              [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE]];
-var currentBoard = null;
+
+var textBoard = [[], 
+                 [],
+                 []];
 
 function preload() {
     game.load.image('o', 'assets/images/o.svg');
@@ -21,10 +25,32 @@ function create() {
     
     //Global Variables
     style = {fill: 'black', font: '40pt Courier'};
-    
-    game.stage.backgroundColor = 0xffffff;
-    graphics.lineStyle(8, 0x000000);
+    game.stage.backgroundColor = 0xFFFFFF;
 
+    drawBoard(graphics);
+    
+    game.input.onDown.add(mouseClicked, this);
+    
+    var rand = Math.random();
+    if (rand < 0.5) 
+        playerTurn = false;
+    else 
+        playerTurn = true;
+    console.log(playerTurn);
+}
+
+function whoGoesFirst() {
+    rand = Math.random();
+    
+    if (rand < 0.5) 
+        return true;
+    else {
+        return false;
+    }
+}
+
+function drawBoard(graphics) {
+    graphics.lineStyle(3, 0x000000);
     
     //Makes a tic tac toe board that is 600 pixels squared
     for (var i = 1; i < 3; i++) {
@@ -33,47 +59,28 @@ function create() {
         
         graphics.moveTo(20, 620 * (i / 3) + 10);
         graphics.lineTo(620, 620 * (i / 3)+ 10);
-    }
-    
-    game.input.onDown.add(mouseClicked, this);
-    
-    playerTurn = true;
-    clearBoard();
-}
-
-function whoGoesFirst() {
-    rand = Math.random();
-    
-    if (rand < 0.5) 
-        return true;
-    else 
-        return false;
+    }   
 }
 
 function update() {
-    
+    if (!playerTurn)
+        opponentTurn();
 }
 
 function opponentTurn() {
-        console.log("opponent Turn");
-        var moveMade = false;
-        while (!moveMade) {
-            randX = Math.floor(Math.random() * 3);
-            randY = Math.floor(Math.random() * 3);
-            
-            console.log(board);
-            console.log("randX: " + randX + " randY: " + randY);
-            console.log("conditional " + (board[randY][randX] == EMPTY_SPACE) + " " + board[randY][randX]);
-            if (board[randY][randX] == EMPTY_SPACE) {
-                setMove("o", randX, randY);
-                moveMade = true;
-            }
-            console.log(board[randY][randX]);
-        }    
+    console.log("opponent Turn");
+    console.log("PT: " + playerTurn);
+    buildTree(board, playerTurn);
 }
 
 function mouseClicked() {
-    if (playerTurn) {
+    if (checkForWin(board, "x") || checkForWin(board, "o") || isFull(board)) {
+        console.log("mouseclick");
+        clearBoard();
+        resetBoard();
+    }
+    else {
+        if (playerTurn) {
         //A 3rd of the board size(600)
         var num = 1/3 * 600;
             //This code checks where you made your move
@@ -82,11 +89,14 @@ function mouseClicked() {
                     if (game.input.x > num * x && game.input.x < num * x + num && game.input.y > num * y && game.input.y < num * y + num) {
                         if (board[y][x] != EMPTY_SPACE)
                             return;
-                        setMove('x', x, y);
+                        console.log(x + " " + y);
+                        makeMove(board, 'x', x, y);
                     }
                 }
             }
         opponentTurn();
+        }
+        //opponentTurn();
     }
 }
 
@@ -95,7 +105,19 @@ function clearBoard() {
     board = [[EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
              [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
              [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE]];
-    moveCount = 0;
+}
+
+function resetBoard() {
+    graphics = game.add.graphics(0, 0);
+    clearBoard();
+    
+    graphics.beginFill(0xFFFFFF);
+    graphics.lineStyle(2, 0xFFFFFF, 1);
+    graphics.drawRect(0, 0, 1000, 660);
+    graphics.endFill();
+    
+    drawBoard(graphics);
+    
 }
 
 function sleep(milliseconds) {
@@ -106,35 +128,44 @@ function sleep(milliseconds) {
 }
 
 //Makes move on board and changes player turn
-function setMove(type, x, y) {
-    board[y][x] = type;
-    moveCount++;
-    game.add.text(200 * x + 100, 200 * y + 100, type, style);
-    console.log("draw");
+function makeMove(board, type, x, y) {
+    console.log(board + " type: " + type + " x: " + x + " y: " + y);
+    setMove(board, type, x, y);
+    textBoard[y][x] = game.add.text(200 * x + 100, 200 * y + 100, type, style);
     playerTurn = !playerTurn;
-    
-    if (checkForWin(board, x, y, type)) 
+    if (checkForWin(board, type)) 
         gameOver(type);
-    
+    //spaces after tie to push 'wins!' concat off screen
+    if (isFull(board))
+        gameOver("tie     ");
+}   
+
+function setMove(board, type, x, y) {
+    //console.log("info: " + type + " " + x + " " + y);
+    board[y][x] = type;
 }
 
-function checkForWin(board, x, y, type) {
+function checkForWin(board, type) {
     //Gets the x coordinate of the last move and
     //checks that column for a win
-    for (var i = 0; i < 3; i++) {
-        if (board[i][x] != type)
-            break;
-        if (i == 2) 
-            return true;
+    for (var y = 0; y < 3; y++) {
+        for (var x = 0; x < 3; x++) {
+            if (board[y][x] != type)
+                break;
+            if (x == 2) 
+                return true;
+        }
     }
     
     //Gets the y coordinate of the last move and
     //checks that row for a win
-    for (var i = 0; i < 3; i++) {
-        if (board[y][i] != type) 
-            break;
-        if (i == 2) 
-            return true;
+    for (var x = 0; x < 3; x++) {
+        for (var y = 0; y < 3; y++) {
+            if (board[y][x] != type) 
+                break;
+            if (y == 2) 
+                return true;
+        }
     }
     
     //If we are on a diagonal
@@ -157,8 +188,112 @@ function checkForWin(board, x, y, type) {
 }
 
 function gameOver(type) {
-    //set to false in order to solve bug of playing after end
-    playerTurn = false;
-    game.add.text(750, 300, type + " wins", style);
-    shutdown();
+    game.add.text(750, 300, type + " wins!", style);
+}
+
+function getMoves(board, depth) {
+    var moves = [];
+    for(var y = 0; y < 3; y++){
+        for (var x = 0; x < 3; x++) {
+            if(board[y][x] == " ")
+                moves.push([y, x]);   
+        }
+    }
+    if (depth == 0)
+        console.log("moves " + moves);
+    return moves;
+}
+
+
+function randomMove(posMoves) {
+    var rand = Math.floor(Math.random() * posMoves.length);
+    return posMoves[rand];
+}
+
+function buildTree(board, playerTurn) {
+    var currPlayer = playerTurn == true ? "x" : "o";
+    MAX_DEPTH = 6;
+    bestMove = [];
+    var alpha = buildTree_r(board, currPlayer, 0);
+    //To make sure we don't crash
+    if (bestMove[0] != null) 
+        makeMove(board, currPlayer, bestMove[1], bestMove[0]);
+}
+
+function buildTree_r(board, currPlayer, depth) {
+    if (depth > MAX_DEPTH) 
+        return 0;
+
+    var otherPlayer = currPlayer == "x" ? "o" : "x";
+
+    if (checkForWin(board, currPlayer)) {
+        //console.log("win for " + currPlayer + "  at depth = " + depth);
+        return 1;
+    }
+    
+    else if (checkForWin(board, otherPlayer)) {
+        //console.log("win for " + otherPlayer + " at depth = " + depth);
+        return -1;
+    }
+    else if (isFull(board)) {
+        //console.log("tie found at depth = " + depth);
+        return 0;
+    }
+
+    var moveList = getMoves(board, depth);
+    var alpha = -1;
+    var saList = [];
+
+    for (var i = 0; i < moveList.length; i++) {
+        var boardCopy = copyBoard(board);
+        
+        setMove(boardCopy, currPlayer, moveList[i][1], moveList[i][0]);
+        
+        
+        var subalpha = -buildTree_r(boardCopy, otherPlayer, depth + 1);
+        if (subalpha == NaN) 
+            console.log("NaN");
+        if (alpha < subalpha)
+            alpha = subalpha;
+
+        //push this subalpha to the subalpha list
+        if (depth == 0)
+            saList.push(subalpha);
+    }
+
+    if (depth == 0) {
+        var posMoves = [];
+        console.log(saList);
+        for (var n = 0; n < saList.length; n++) {
+            if (saList[n] == alpha)
+                posMoves.push(moveList[n]);
+        }
+        console.log("posMoves " + posMoves);
+        bestMove = randomMove(posMoves);
+        console.log("alpha " + alpha);
+        console.log("bestMove: (" + bestMove[1] + ", " + bestMove[0] + ")");
+    }
+    return alpha;   
+}
+
+function isFull(board) {
+    for (x = 0; x < 3; x++) {
+        for (y = 0; y < 3; y++) {
+            if (board[y][x] == " ")
+                return false;
+        }
+    }
+    return true;
+}
+
+function copyBoard(board) {
+    var boardCopy = [[EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
+             [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
+             [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE]];
+    for (x = 0; x < 3; x++) {
+        for (y = 0; y < 3; y++) {
+            boardCopy[y][x] = board[y][x]
+        }
+    }
+    return boardCopy
 }
